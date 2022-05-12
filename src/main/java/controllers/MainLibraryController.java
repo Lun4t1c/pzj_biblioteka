1eec25d2c1be389664480f74b588c115d269f58c
Artur Grabowski <UL0234806@edu.uni.lodz.pl>
2022-05-12 09:35:48 +0200

    Blergh

--- src/main/java/controllers/MainLibraryController.java
+++ src/main/java/controllers/MainLibraryController.java
@@ -2,15 +2,23 @@ package controllers;
 
 import com.pzj.View;
 import com.pzj.ViewSwitcher;
+import dao.DataAcces;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import models.BookModel;
 
 public class MainLibraryController {
     //region FXML Controls
     @FXML
     private TextField SearchTextField;
     //endregion
+
+
+    //region Attributes
+    ObservableList<BookModel> books = DataAcces.getAllBooks();
+    //endregion]
 
 
     //region Methods
