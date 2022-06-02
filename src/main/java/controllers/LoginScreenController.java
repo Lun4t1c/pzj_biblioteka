4f52494c424df7c2b73f5657f27093da2b0db9fb
Artur Grabowski <UL0234806@edu.uni.lodz.pl>
2022-06-02 18:32:47 +0200

    Dummy books

---
+++ src/main/java/controllers/LoginScreenController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginScreenController {
+    //region FXML controls
+    @FXML
+    private TextField LoginTextField;
+
+    @FXML
+    private TextField PasswordTextField;
+    //endregion
+
+
+    //region Attributes
+
+    //endregion
+}
