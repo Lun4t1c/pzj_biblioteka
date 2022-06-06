9e738f214bfe4891016dba25e0676c01d216839a
Mikołaj Kliche <mikolaj.kliche@unilodz.eu>
2022-06-06 14:33:35 +0200

    nowe lajałty

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/*
+@FXML
+private TextField loginTextField;
+
+@FXML
+private PasswordField paswordPasswordField
+
+@FXML
+private Button loginbtn;
+
+@FXML
+private Button registerbtn;
+
+public class LoginController {
+}*/
