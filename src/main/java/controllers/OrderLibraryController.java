adcac7369a9bdbb6066efbe9fd0a56ebb363daa1
Mikołaj Kliche <mikolaj.kliche@unilodz.eu>
2022-06-07 11:25:42 +0200

    nowe lajałty

---
+++ src/main/java/controllers/OrderLibraryController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+@FXML
+private TextField searchTextField;
+
+@FXML
+private ListView ordersListView
+
+@FXML
+private TextField titletf;
+
+@FXML
+private Label copyValuelbl;
+
+@FXML
+private Label reservationDateValuelbl;
+
+@FXML
+private Label reservationTermValuelbl;
+
+@FXML
+private Label borrowDateValuelbl;
+
+@FXML Label borrowTermValuelbl;
+
+@FXML
+private Label returnDateValuelbl;
+
+@FXML
+private Button cancelButton;
+
+@FXML
+private Button confirmButton;
+
+public class OrderLibraryController {
+}
